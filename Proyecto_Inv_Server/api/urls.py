5295d59842f7e11064339bebdbbd37bf1ff0b227
restax099 <restax099@gmail.com>
2025-08-24 22:08:28 -0700

    modificando nombre API

--- Proyecto_Inv_Server/api/urls.py
+++ Proyecto_Inv_Server/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('consulta_general_test/', views.consulta_general, name='consulta-general-test'),
+    path('consulta_general_test/', views.consulta_general_test, name='consulta-general-test'),
 ]
