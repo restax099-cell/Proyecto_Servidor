d5105c4b259b8f7b6a2dbdc75dea65c9037d0b1c
restax099 <restax099@gmail.com>
2025-08-27 13:34:13 -0700

    Agregando utils, para separar algoritmos logicos de request api de Django y mejorando detección de codigo de barras con opencv

--- Proyecto_Inv_Server/api/views.py
+++ Proyecto_Inv_Server/api/views.py
@@ -2,10 +2,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .models import BarCode
 import base64
-from io import BytesIO
-from PIL import Image
-from pyzbar.pyzbar import decode
 import json
+from .utils.barcode_utils import decode_barcodes
+
 
 @csrf_exempt
 def add_barcode(request):
@@ -19,28 +18,22 @@ def add_barcode(request):
             if not img_base64:
                 return JsonResponse({"error": "No se envió la imagen"}, status=400)
 
-            # Decodificar la imagen Base64
+            # Decodificar imagen Base64 a bytes
             image_bytes = base64.b64decode(img_base64)
-            image = Image.open(BytesIO(image_bytes))
 
-            # Leer códigos en la imagen
-            decoded_objects = decode(image)
-            if not decoded_objects:
-                return JsonResponse({"error": "No se detectó ningún código"}, status=400)
-
-            # Filtrar solo códigos de barra (excluyendo QR)
-            barcodes = [obj for obj in decoded_objects if obj.type != "QRCODE"]
+            # Detectar códigos de barra (ignora QR)
+            barcodes = decode_barcodes(image_bytes)
 
             if not barcodes:
-                return JsonResponse({"error": "Solo se detectaron QR, no códigos de barra"}, status=400)
+                return JsonResponse({"error": "No se detectó ningún código de barras"}, status=400)
 
-            # Tomar el primer código de barra válido
+            # Tomar el primero válido
             barcode_data = barcodes[0].data.decode("utf-8")
 
             barcode = BarCode.objects.create(
                 name=name,
-                img=image_bytes,   # Guardamos binario
-                code=barcode_data, 
+                img=image_bytes,
+                code=barcode_data,
                 status=1
             )
 
