669568e295a348c08ae5cb61398f6d4577fa7d68
restax099 <restax099@gmail.com>
2025-08-30 20:49:08 -0700

    agregando directamente la ruta path del dirver al algoritmo de selenium

--- Proyecto_Inv_Server/api/utils/code_utils.py
+++ Proyecto_Inv_Server/api/utils/code_utils.py
@@ -1,31 +1,43 @@
+# my_app/services/folio_service.py
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def get_folio(url):
-    opciones = Options()
-    opciones.add_argument('--headless')  # Ejecutar en segundo plano
-    driver = webdriver.Chrome(options=opciones)
+def obtener_folio_selenium(url):
 
-    driver.get(url)
+    opciones = Options()
+    opciones.add_argument('--headless')
+    opciones.add_argument('--no-sandbox')
+    opciones.add_argument('--disable-dev-shm-usage')
+    
+
+    driver_path = '/usr/local/bin/chromedriver' 
 
     try:
+  
+        driver = webdriver.Chrome(executable_path=driver_path, options=opciones)
+    except Exception as e:
+        print(f"Error al iniciar el driver de Chrome: {e}")
+        return "Error al iniciar el navegador"
+
+    try:
+        driver.get(url)
         
-        cel_label = WebDriverWait(driver, 10).until(
+        celda_label = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//td[span[contains(text(),'Folio:')]]"))
         )
 
-       
-        cel_value = cel_label.find_element(By.XPATH, "following-sibling::td")
-        folio = cel_value.text.strip()
-
+        celda_valor = celda_label.find_element(By.XPATH, "following-sibling::td")
+        folio = celda_valor.text.strip()
+        
     except Exception as e:
-        print("Error al extraer el folio:", e)
-        folio = None
-
-    driver.quit()
+        print(f"Error al extraer el folio: {e}")
+        folio = "Folio no encontrado"
+        
+    finally:
+        driver.quit()
+        
     return folio
-
-
