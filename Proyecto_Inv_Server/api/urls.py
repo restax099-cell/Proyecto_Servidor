59f583e47103f0def54fc056f331a5817ebeaf33
restax099 <restax099@gmail.com>
2025-09-18 13:53:23 -0700

    Agregando nuevas apis para obtener tablas completas de xml data y xml head

--- Proyecto_Inv_Server/api/urls.py
+++ Proyecto_Inv_Server/api/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 from .views.codes_views import get_folio_from_url
-from .views.xml_views import get_xml_data
+from .views.xml_views import (
+    get_xml_file,
+    get_xml_data,
+    get_xml_head
+)
 from .views.prueba_views import get_prueba
 
 
 urlpatterns = [
     
     path('get-folio/', get_folio_from_url, name='get-folio'),
-    path('get-xml/', get_xml_data, name='get-xml-data'),
+    
+    path('get-xml', get_xml_file, name='get-xml-file'),
+    path('get-xml-data/', get_xml_data, name='get-xml-data'),
+    path('get-xml-head/', get_xml_head, name='get-xml-head'),
+
     path('get-prueba/', get_prueba, name='get-prueba'),
 ]
--- Proyecto_Inv_Server/api/views/xml_views.py
+++ Proyecto_Inv_Server/api/views/xml_views.py
@@ -1,10 +1,31 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 import json
 
-from ..models.xml_models import VlxSatCfdiRaw
+from ..models.xml_models import (
+    VlxSatCfdiRaw,
+    VlxDataXml,
+    VlxTotalDataXml
+)
 
+
+
+@csrf_exempt
+def get_xml_file(request):
+    if request.method == "GET":
+        uuid = request.GET.get("uuid")
+        if not uuid:
+            return JsonResponse({"error": "Falta el parámetro uuid"}, status=400)
+        
+        try:
+            record = VlxSatCfdiRaw.objects.get(uuid=uuid)
+            return JsonResponse({"xmlcontent": record.xmlcontent})
+        except VlxSatCfdiRaw.DoesNotExist:
+            return JsonResponse({"error": "UUID no encontrado"}, status=404)
+    else:
+        return JsonResponse({"error": "Método no permitido"}, status=405)
 
 
 @csrf_exempt
@@ -15,9 +36,26 @@ def get_xml_data(request):
             return JsonResponse({"error": "Falta el parámetro uuid"}, status=400)
         
         try:
-            record = VlxSatCfdiRaw.objects.get(uuid=uuid)
-            return JsonResponse({"xmlcontent": record.xmlcontent})
-        except VlxSatCfdiRaw.DoesNotExist:
+            record = VlxDataXml.objects.get(uuid=uuid)
+            record_dict = model_to_dict(record) 
+            return JsonResponse(record_dict)
+        except VlxDataXml.DoesNotExist:
+            return JsonResponse({"error": "UUID no encontrado"}, status=404)
+    else:
+        return JsonResponse({"error": "Método no permitido"}, status=405)
+    
+@csrf_exempt
+def get_xml_head(request):
+    if request.method == "GET":
+        uuid = request.GET.get("uuid")
+        if not uuid:
+            return JsonResponse({"error": "Falta el parámetro uuid"}, status=400)
+        
+        try:
+            record = VlxTotalDataXml.objects.get(uuid=uuid)
+            record_dict = model_to_dict(record) 
+            return JsonResponse(record_dict)
+        except VlxTotalDataXml.DoesNotExist:
             return JsonResponse({"error": "UUID no encontrado"}, status=404)
     else:
         return JsonResponse({"error": "Método no permitido"}, status=405)
