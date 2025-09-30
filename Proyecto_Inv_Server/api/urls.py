3e79f43aa2102309bbb86e9f858a1e97f6e7c0fc
restax099 <restax099@gmail.com>
2025-09-30 13:01:24 -0700

    Cambiando nombres de apis del api/urls.py

--- Proyecto_Inv_Server/api/urls.py
+++ Proyecto_Inv_Server/api/urls.py
@@ -17,9 +17,9 @@ urlpatterns = [
     path('get-xml-data/', xml_views.get_xml_data, name='get-xml-data'),
     path('get-xml-head/', xml_views.get_xml_head, name='get-xml-head'),
 
-    path('get_all_raw/', xml_views.get_all_raw_cfdi, name='get_all_raw_cfdi'),
-    path('get_all_data/', xml_views.get_all_data_xml, name='get_all_data_xml'),
-    path('get_all_total/', xml_views.get_all_total_data_xml, name='get_all_total_data_xml'),
+    path('get-all-raw/', xml_views.get_all_raw_cfdi, name='get-all-raw-cfdi'),
+    path('get-all-data/', xml_views.get_all_data_xml, name='get-all-data-xml'),
+    path('get-all-total/', xml_views.get_all_total_data_xml, name='get-all-total-data-xml'),
 
     # Rutas de prueba_views
     path('get-prueba/', prueba_views.get_prueba, name='get-prueba'),
