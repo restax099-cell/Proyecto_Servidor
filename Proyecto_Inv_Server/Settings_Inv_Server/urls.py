17d56b5f89bb710a2391b99e6d61345455820bc5
restax099 <restax099@gmail.com>
2025-09-30 15:10:06 -0700

    Corrigiendo sintaxis

--- Proyecto_Inv_Server/Settings_Inv_Server/urls.py
+++ Proyecto_Inv_Server/Settings_Inv_Server/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', web_views.home, name='home'),
-    path('admin-panel', web_views.admin_panel, name='admin_panel'),
+    path('admin-panel/', web_views.admin_panel, name='admin_panel'),
 ]
