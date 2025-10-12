dcafb8bde861bcf4c757bd211da1404019831a0e
restax099 <restax099@gmail.com>
2025-10-12 16:51:08 -0700

    Agregando Tokens para consumo de Apis protegidas

--- Proyecto_Inv_Server/Settings_Inv_Server/settings.py
+++ Proyecto_Inv_Server/Settings_Inv_Server/settings.py
@@ -166,6 +166,7 @@ LOGGING = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAdminUser',
--- Proyecto_Inv_Server/api/admin.py
+++ Proyecto_Inv_Server/api/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rest_framework.authtoken.models import Token
 
 from .models.xml_models import (
     VlxSatCfdiRaw, 
@@ -8,7 +9,10 @@ from .models.xml_models import (
 
 admin.site.register([VlxSatCfdiRaw, VlxDataXml, VlxTotalDataXml])
 
-
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('key', 'user', 'created')
+    search_fields = ('user__username',)
 
 #from .models import BarCode
 
